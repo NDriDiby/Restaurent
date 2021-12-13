3b0cad7cb373d29d28b6f66e71aaf60149eb5ad9
N'Dri Diby <ndiby65@gmail.com>
2021-12-12 23:10:00 -0800

    New settings

--- Restaurent/settings.py
+++ Restaurent/settings.py
@@ -135,16 +135,16 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-   ]
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#    ]
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
  
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
