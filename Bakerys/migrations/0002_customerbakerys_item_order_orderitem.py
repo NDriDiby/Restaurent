804b8f72b54344d7e38f08828a9f90f717fab28e
N'Dri Diby <ndiby65@gmail.com>
2021-12-15 01:58:53 -0800

    ok

--- Bakerys/migrations/0002_customerbakerys_item_order_orderitem.py
+++ Bakerys/migrations/0002_customerbakerys_item_order_orderitem.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('Bakerys', '0001_initial'),
-        ('Bakerys','__last__')
+        ('Bakerys', '0003_rename_customerbakerys_customerbekerys')
     ]
 
     operations = [
