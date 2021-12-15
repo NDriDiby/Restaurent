a4065b9d7123f67cc43616ccf25a42b9d3dd4337
N'Dri Diby <ndiby65@gmail.com>
2021-12-15 00:19:58 -0800

    next try

--- Restaurent/settings.py
+++ Restaurent/settings.py
@@ -135,13 +135,13 @@ USE_TZ = True
 LOGIN_URL = 'login'
 
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
    ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 MEDIA_URL = '/media/'
