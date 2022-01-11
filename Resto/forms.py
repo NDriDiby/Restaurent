f26fb8a5952c651c63877def7359406389aa5804
N'Dri Diby <ndiby65@gmail.com>
2022-01-11 11:36:26 -0800

    Working on Login Logic

--- Resto/forms.py
+++ Resto/forms.py
@@ -7,6 +7,7 @@ from .forms import models
 from.models import Item,ItemChoices,ItemChoiceCategory,IventoryItem
 from django.contrib.auth.models import AbstractUser, User,AbstractBaseUser
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 
@@ -17,6 +18,21 @@ class CustomerForm(UserCreationForm):
    prenom = forms.CharField(label = 'Prenom')
    email = forms.EmailField(label = 'Email')
    phone_number = forms.CharField(label = 'Phone Number')
+   
+   # error_messages = {
+   #      'password_mismatch': _("The two password fields didn't match."),
+   #  }
+   
+   # username = forms.CharField(label=_("Username"),
+   #      help_text=_("Required. 100 characters or fewer. Letters, digits and @/./+/-/_ only."))
+   
+   # password1 = forms.CharField(label=_("Password"),
+   #      widget=forms.PasswordInput)
+  
+
+   # password2 = forms.CharField(label=_("Password confirmation"),
+   #      widget=forms.PasswordInput,
+   #      help_text=_("Entrez le même password, pour verification."))
    
 
    class Meta:
