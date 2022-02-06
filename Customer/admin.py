6af6f43114f88ebf23c088ce45512f9b1273a299
N'Dri Diby <ndiby65@gmail.com>
2022-02-05 23:45:15 -0800

    Database migrations

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import phone
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('numb')
+admin.site.register(phone,PhoneAdmin)
+
 # Register your models here.
