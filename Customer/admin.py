afd8060416fab7f9e962b8c7bf4e9266bbba4086
N'Dri Diby <ndiby65@gmail.com>
2022-02-06 16:01:18 -0800

    delete cust phone

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
-from .models import phone
 
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ('id','numb')
-admin.site.register(phone,PhoneAdmin)
+
+
 
 # Register your models here.
--- Customer/models.py
+++ Customer/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-class phone(models.Model):
-    numb = models.IntegerField(blank=True)
+
 
 # Create your models here.
