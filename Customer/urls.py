f1ac717729c93956a172efe5be85587d1a1d25dd
N'Dri Diby <ndiby65@gmail.com>
2022-02-07 01:43:07 -0800

    sending email looks good

--- Customer/urls.py
+++ Customer/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/',cust_view.Login,name = 'login'),
     path('logout/',cust_view.Logout,name = 'logout'),
     path('password_reset/',auth_view.PasswordResetView.as_view(template_name = 'Customer/password_reset.html',
-    html_email_template_name = "Customer/password_reset_email.txt"),name = 'password_reset'),
+    html_email_template_name = "Customer/password_reset_email.txt",extra_email_context ={'page':'password_reset/'}),name = 'password_reset'),
     path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='Customer/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name="Customer/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_view.PasswordResetCompleteView.as_view(template_name='Customer/password_reset_complete.html'), name='password_reset_complete'),   
