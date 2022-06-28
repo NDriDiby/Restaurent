f2fc9c7756371ffa56f2b6efbbc538307fd4f500
N'Dri Diby <47793843+NDriDiby@users.noreply.github.com>
2022-06-27 18:50:29 -0700

    celery is ready to go

--- Restaurent/celery.py
+++ Restaurent/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurent.settings')
@@ -20,3 +21,14 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+    
+
+
+#Scheduling Task
+app.conf.beat_schedule = {
+    "aa-task": {
+        "task": "Resto.tasks.add",
+        "schedule": crontab(minute="*"),
+        'args': (16, 16)
+    }
+}
--- Resto/tasks.py
+++ Resto/tasks.py
@@ -1,6 +1,9 @@
-
 from celery import shared_task
+from celery.schedules import crontab
+
+
 
 @shared_task
 def add(x, y):
     return x + y
+
