22b2c47e62ac33d5829eb6e5bf82aea0cd98914a
N'Dri Diby <47793843+NDriDiby@users.noreply.github.com>
2022-09-10 22:11:23 -0700

    keep going

--- Restaurent/celery.py
+++ Restaurent/celery.py
@@ -1,5 +1,5 @@
 import os
-
+from django.conf import settings
 from celery import Celery
 from celery.schedules import crontab
 import os, ssl
@@ -18,10 +18,10 @@ app = Celery('Restaurent',broker_use_ssl = {
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django apps.
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
