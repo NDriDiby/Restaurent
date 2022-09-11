4059fd2e68fdc7ff6c744ec90861b0338fae54cf
N'Dri Diby <47793843+NDriDiby@users.noreply.github.com>
2022-09-10 21:36:17 -0700

    celery done

--- Restaurent/celery.py
+++ Restaurent/celery.py
@@ -1,13 +1,13 @@
 import os
 
-import celery
+from celery import Celery
 from celery.schedules import crontab
 import os, ssl
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurent.settings')
 
-app = celery.Celery('Restaurent',broker_use_ssl = {
+app = Celery('Restaurent',broker_use_ssl = {
         'ssl_cert_reqs': ssl.CERT_NONE
      },
      redis_backend_use_ssl = {
